ece019b7949e3b21d0f6b98a4a6ba43349c1ca12
MotKyz <kuznetsov.matvey.2005@mail.ru>
2024-12-13 01:24:28 +0500

    task04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,42 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Task04Main {
 
     public static void main(String[] args) {
 
-        // your implementation here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<String> list = br.lines()
+                .map(String::toLowerCase)
+                .flatMap(x -> Stream.of(x.split("[^а-яёa-z]")))
+                .filter(t -> !t.isEmpty())
+                .collect(Collectors.toList());
 
+        Map<String, Integer> map = new TreeMap<>();
+        for(int i = 0; i < list.size(); i++) {
+            if(map.containsKey(list.get(i))) {
+                map.replace(list.get(i), map.get(list.get(i)) + 1);
+            }
+            else {
+                map.put(list.get(i), 1);
+            }
+        }
+
+        map.entrySet()
+        .stream()
+        .sorted(Map.Entry.comparingByKey())
+        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(10)
+                .forEach(e -> System.out.print(e.getKey() + "\n"));
     }
-
 }
