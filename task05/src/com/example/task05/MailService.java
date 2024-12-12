13e4be682d14b5dec882ed3c2f7d2ed246007261
MotKyz <kuznetsov.matvey.2005@mail.ru>
2024-12-13 03:11:45 +0500

    task05

---
+++ task05/src/com/example/task05/MailService.java
@@ -0,0 +1,24 @@
+package com.example.task05;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+public class MailService<T> implements Consumer<Mail<T>> {
+
+    private final Map<String, List<T>> mailBox = new HashMap<String, List<T>>() {
+        @Override
+        public List<T> get(Object key) {
+            return super.getOrDefault(key, Collections.emptyList());
+        }
+    };
+
+    @Override
+    public void accept(Mail<T> mail) {
+        mailBox.computeIfAbsent(mail.getTo(), k -> new ArrayList<>())
+                .add(mail.getContent());
+    }
+
+    public Map<String, List<T>> getMailBox() {
+        return mailBox;
+    }
+}
